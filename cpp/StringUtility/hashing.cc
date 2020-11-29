43b3cded2118f2c49191d2c37dd66d5e65014da7
Yik Chun Wong <ianwong1999@yahoo.com.hk>
2020-11-29 16:51:54 +0800

    Update hashing.cc
    1. line 10 should be computing the pw vector instead of hs
    2. line 13 should be accumulating hs[i + 1] instead of hs[i - 1]

--- cpp/StringUtility/hashing.cc
+++ cpp/StringUtility/hashing.cc
@@ -7,10 +7,10 @@ struct Hashing {
     hs.assign(n + 1, 0);
     pw.assign(n + 1, 1);
     for (int i = 1; i <= n; ++i) {
-      hs[i] = seed * hs[i - 1];
+      pw[i] = seed * pw[i - 1];
     }
     for (int i = n - 1; i >= 0; --i) {
-      hs[i] = seed * hs[i - 1] + s[i];
+      hs[i] = seed * hs[i + 1] + s[i];
     }
   }
   ULL get_hash(int l, int r) {// [l, r)
